599c6331027186865efb9edefe789c5b7f7c297d
jabapo <jabapo@umich.edu>
2023-02-13 21:01:52 -0500

    updated education, will update projects soon and revise skills

--- src/app/pages/pages.ts
+++ src/app/pages/pages.ts
@@ -1,6 +1,6 @@
 export const pages = [
   { index: 0, name: 'overview.md', route: '/overview' },
-  { index: 2, name: 'experience.md', route: '/experience' },
-  { index: 3, name: 'education.md', route: '/education' },
-  { index: 4, name: 'projects.md', route: '/projects' },
+  { index: 1, name: 'experience.md', route: '/experience' },
+  { index: 2, name: 'education.md', route: '/education' },
+  { index: 3, name: 'projects.md', route: '/projects' },
 ];
